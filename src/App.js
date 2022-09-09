75154e3e1fd50119c3c72b5be7a226858f907548
mufid048 <20it048@charusat.edu.in>
2022-09-09 13:22:06 +0530

    Added Student Listing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Footer from './components/Footer';
 import NavigationBar from './components/NavigationBar';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Container>
       <Student />
       <StudentList />
+      </Container>
       <Footer/>
     </div>
   );
