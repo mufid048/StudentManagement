ea1f110cb750f76cba2440a61125638b8777cd05
mufid048 <20it048@charusat.edu.in>
2022-09-09 12:43:06 +0530

    Created initial UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import Student from './components/Student';
+import StudentList from './components/StudentList';
 
 function App() {
   return (
     <div className="App">
-     <NavigationBar/>
+     <Student/>
+     <StudentList/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div></div>
+    // <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    //         <Container>
+    //             <Navbar.Brand href="#home">Student Management System</Navbar.Brand>
+    //             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    //             <Navbar.Collapse id="responsive-navbar-nav">
+    //                 <Nav className="me-auto">
+    //                     <Nav.Link href="#features">Add Student</Nav.Link>
+    //                     <Nav.Link href="#pricing">View Student</Nav.Link>
+    //                 </Nav>
+    //             </Navbar.Collapse>
+    //         </Container>
+    //     </Navbar>
+  )
+}
