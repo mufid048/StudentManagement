5ca69a9ec17ac4c00f902fb91a6b6905d74e7e42
mufid048 <20it048@charusat.edu.in>
2022-09-12 13:39:43 +0530

    Added delete

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -16,6 +16,15 @@ export default function StudentList() {
       .then((response) => setStudents(response.data))
       .catch((error) => alert(error));
   };
+  let deleteStudent = (studentId) => {
+    axios.delete("http://localhost:8080/student/"+studentId)
+    .then(response=> {
+      if (response.data !== null){
+        alert("Record Deleted Successfully");
+
+      }
+    })
+  }
   return (
     <div className="my-3">
       <Container>
@@ -46,7 +55,7 @@ export default function StudentList() {
                   <td>
                     <ButtonGroup>
                       <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
-                      <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
                     </ButtonGroup>
                   </td>
                 </tr>
