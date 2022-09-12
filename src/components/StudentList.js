ab856eec1eb1cf0a7e0818d6a4f1b68c6aa55f6c
mufid048 <20it048@charusat.edu.in>
2022-09-12 13:33:50 +0530

    Add edit/delete student

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Table } from "react-bootstrap";
+import { Card, Container, Table, ButtonGroup, Button } from "react-bootstrap";
 import axios from "axios";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 export default function StudentList() {
   const [students, setStudents] = useState([]);
 
@@ -28,12 +29,13 @@ export default function StudentList() {
                 <th>Student Id</th>
                 <th>Student Name</th>
                 <th>Student Address</th>
+                <th>Edit/Delete</th>
               </tr>
             </thead>
             <tbody>
               {students.length === 0 ? (
                 <tr>
-                  <td colSpan={3}>{students.length} Studnets Available!!!</td>
+                  <td colSpan={3}>{students.length} Students Available!!!</td>
                 </tr>
               ) : (
                 students.map((student)=>
@@ -41,6 +43,12 @@ export default function StudentList() {
                   <td>{student.id}</td>
                   <td>{student.name}</td>
                   <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
+                      <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                    </ButtonGroup>
+                  </td>
                 </tr>
                 )
               )}
